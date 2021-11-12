2bbb16276233bdf6965e9b74149651180e418ed7
tu-ngo <tu.ngo@gooddata.com>
2021-11-12 10:14:47 +0700

    Update Readme, change config to run vs kafka on PI

--- kaka-message/README.md
+++ kaka-message/README.md
@@ -1,5 +1,17 @@
 # SpringBoot application
-Run the application and use curl to send some data:
+
+Build:
+`mvn clean  package`
+
+Run
+`mvn spring-boot:run`
+
+Run the application and use curl to send some data to input topic `rubik-hack.input`:
 
 `$curl -X POST -d '{"id": "id", "name": "name", "title": "title", "price": 695783, "date": "2015-01-01" }'  -H 'Content-Type: application/json' http://localhost:8080/send/opportunity/`
 
+The consumer will read the message from `rubik-hack.input`,
+then ETL engine will transform above message by the [script](src/main/resources/transform_script.js)
+After transforming, message will be pushed to topic `rubik-hack.output` 
+TODO: implement scheduler to load data from topic `rubik-hack.output` to ADS table via JDBC with the command:
+`COPY [TABLE_NAME] SOURCE KafkaSource ...`
--- kaka-message/src/main/java/com/gooddata/rubik/hackathon/goodload/Controller.java
+++ kaka-message/src/main/java/com/gooddata/rubik/hackathon/goodload/Controller.java
@@ -1,6 +1,8 @@
 package com.gooddata.rubik.hackathon.goodload;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gooddata.rubik.hackathon.goodload.common.Opportunity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,13 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
     private final Logger logger = LoggerFactory.getLogger(Controller.class);
+
     @Autowired
     private KafkaTemplate<Object, Object> template;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
 
     @PostMapping(path = "/send/opportunity/")
     public void sendOpportunity(@RequestBody final Opportunity opportunity) {
         logger.info("Start to process: " + opportunity.getName());
-        this.template.send("rubik-hack.input", opportunity .toString());
+        try {
+            this.template.send("rubik-hack.input", objectMapper.writeValueAsString(opportunity));
+        } catch (JsonProcessingException e) {
+            logger.warn("Error while write to json", e);
+        }
     }
 
 }
